2a8d77cf7d388d5957c6e5e3acd7eb6aaabb8800
woblerr <antkurochkin@gmail.com>
2025-07-01 12:05:12 +0300

    Set correct wal_level for different gpdb versions.

--- docker/files/start_gpdb.sh
+++ docker/files/start_gpdb.sh
@@ -270,8 +270,6 @@ initialize_and_start_gpdb() {
         fi
         if [ "${GREENPLUM_WALG_ENABLE}" == "true" ]; then
             echo "INFO - Set parameters fot WAL archiving"
-            echo "INFO - gpconfig -c wal_level -v archive"
-            USER=${GREENPLUM_USER} gpconfig -c wal_level -v archive --skipvalidation
             echo "INFO - gpconfig -c archive_mode -v on"
             USER=${GREENPLUM_USER} gpconfig -c archive_mode -v on
             echo "INFO - gpconfig -c archive_command -v '/bin/true'"
@@ -279,8 +277,15 @@ initialize_and_start_gpdb() {
             # This is necessary to avoid errors
             # Use init script to set actual archive_command
             USER=${GREENPLUM_USER} gpconfig -c archive_command -v "'/bin/true'"
-            echo "INFO - psql ${GREENPLUM_DATABASE_NAME} -t -c \"CREATE EXTENSION IF NOT EXISTS gp_pitr;\" | xargs"
-            psql ${GREENPLUM_DATABASE_NAME} -t -c "CREATE EXTENSION IF NOT EXISTS gp_pitr;" | xargs
+            if [ "${gp_major_version}" == "6" ]; then
+                echo "INFO - gpconfig -c wal_level -v archive"
+                USER=${GREENPLUM_USER} gpconfig -c wal_level -v archive --skipvalidation
+                echo "INFO - psql ${GREENPLUM_DATABASE_NAME} -t -c \"CREATE EXTENSION IF NOT EXISTS gp_pitr;\" | xargs"
+                psql ${GREENPLUM_DATABASE_NAME} -t -c "CREATE EXTENSION IF NOT EXISTS gp_pitr;" | xargs
+            else
+                echo "INFO - gpconfig -c wal_level -v replica"
+                USER=${GREENPLUM_USER} gpconfig -c wal_level -v replica --skipvalidation
+            fi
         fi
         # Configure pg_hba
         echo "INFO - Configure pg_hba.conf"
